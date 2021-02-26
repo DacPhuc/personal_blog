6bc41a4bac29752f8fae563528edadeea9ea3be7
DacPhuc <dacphucng0704@gmail.com>
2021-02-27 00:49:39 +0700

    update layout blog

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,13 @@
 import logo from "../logo.svg";
 import "../App.css";
+import Typography from "@material-ui/core/Typography";
 
 const Main = () => {
   return (
     <div className="App">
+      <Typography className="App-hi" variant="h6">
+        HELLO
+      </Typography>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>My first blog</p>
